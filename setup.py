fae845ffd338049f404f4bb2f200fc7aa06e898a
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-10-28 14:23:02 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,5 @@ setup(
     author="Ansh Vachhani",
     description="Custom Telegram TDLib utilities and downloader",
     packages=find_packages(),
-    install_requires=[
-        "tgcrypto",  # Always install latest tgcrypto
-        "git+https://github.com/TelegramPlayground/pyrogram.git"
-    ],
     python_requires=">=3.10",
 )
