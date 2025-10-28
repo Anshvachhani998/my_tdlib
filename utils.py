4fe2ce3492f877e4704236adc6ee6a1a5d765829
Anshvachhani998 <96714394+Anshvachhani998@users.noreply.github.com>
2025-10-28 14:00:04 +0530

    Create utils.py

---
+++ utils.py
@@ -0,0 +1,18 @@
+# my_tdlib/utils.py
+def format_size(bytes):
+    for unit in ['B', 'KB', 'MB', 'GB', 'TB']:
+        if bytes < 1024.0:
+            return f"{bytes:.2f} {unit}"
+        bytes /= 1024.0
+    return f"{bytes:.2f} PB"
+
+
+def format_time(seconds):
+    if seconds < 60:
+        return f"{int(seconds)}s"
+    elif seconds < 3600:
+        return f"{int(seconds // 60)}m {int(seconds % 60)}s"
+    else:
+        hours = int(seconds // 3600)
+        minutes = int((seconds % 3600) // 60)
+        return f"{hours}h {minutes}m"
